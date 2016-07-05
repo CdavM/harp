8d58ddb3f9e0770f1e36edf7bbf2b0d8c8150239
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-07-05 08:52:30 -0700

    Storing L1 credits in database.

--- server/main.js
+++ server/main.js
@@ -120,7 +120,11 @@ Meteor.methods({
                 }
                 var slider_difference = Number(post.answer['slider' + slider_idx][0]) - existing_entry['initial_slider'+slider_idx];
                 var slider_relative_diff = slider_difference / existing_entry['radius'];
-                answers_value['slider'+slider_idx+"_credits"] = Math.pow(slider_relative_diff, 2);
+                if (current_question == 0) {
+                    answers_value['slider' + slider_idx + "_credits"] = Math.pow(slider_relative_diff, 2);
+                } else if (current_question == 3) {
+                    answers_value['slider' + slider_idx + "_credits"] = Math.abs(slider_relative_diff);
+                }
             }
             if (post.answer['option']){
                 // only for the comparison mechanism.
