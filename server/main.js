000770709be46b2ad0acf5db1e44e14ad0e8c567
nikhgarg <nikhgarg93@gmail.com>
2016-07-27 17:22:05 -0700

    fixed typo

--- server/main.js
+++ server/main.js
@@ -293,7 +293,7 @@ Meteor.methods({
                 
                 var radius_fn = function (previous_participants) {
                     //return 50.0/(Math.floor(previous_participants/10.0)+1);
-                    return 35.0/max(1, Math.ceil((previous_participants - 45.0)/10.0) + 1.0);
+                    return 35.0/Math.max(1, Math.ceil((previous_participants - 45.0)/10.0) + 1.0);
                 };
                 var radius_val = radius_fn(Questions.findOne({"question_ID": next_question}).previous_participants);
                 if ([0, 4, 3, 7].indexOf(next_question) > -1){
